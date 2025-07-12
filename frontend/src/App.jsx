40dca145c1c2912a66327d457e28f97ad67133e3
sombit-k <sombitkarmakar018@gmail.com>
2025-07-12 10:31:34 +0530

    feat: Add Header component to App for improved layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button"
 import AdminPage from "./pages/AdminPage"
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
+      <Header />
       <AdminPage />
     </div>
   );
