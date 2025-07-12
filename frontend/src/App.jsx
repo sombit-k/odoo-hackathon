3a0d2c3b487fae06ea504c76a17a043d0b425dba
sombit-k <sombitkarmakar018@gmail.com>
2025-07-12 16:07:45 +0530

    refactor: remove unused Button import from App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { Button } from "@/components/ui/button";
 import AdminPage from "./pages/AdminPage";
 import Navbar from "./components/Navbar";
 import { Routes, Route, Navigate } from "react-router-dom";
