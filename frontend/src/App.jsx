69aa87ce9ddbfde108d75dfe3dd5c774e7b8fa6a
somu chaulya <somuchaulya8@gmail.com>
2025-07-12 10:21:04 +0530

    Add Header component and integrate Clerk authentication with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button"
-
+import Header from "./components/Header.jsx";
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div>
+      <Header />
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
